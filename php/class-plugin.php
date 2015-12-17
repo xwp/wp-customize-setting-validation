08b1dbee2797c5184a30a3fd4e7a8c1daca5ab04
Weston Ruter <weston@xwp.co>
2015-12-16 22:21:57 -0800

    Print JS templates earlier

--- php/class-plugin.php
+++ php/class-plugin.php
@@ -44,7 +44,7 @@ class Plugin extends Plugin_Base {
 		// Priority is set to 100 so that plugins can attach validation-sanitization filters at default priority of 10.
 		add_action( 'customize_validate_settings', array( $this, 'validate_settings' ), 100 );
 
-		add_action( 'customize_controls_print_footer_scripts', array( $this, 'print_templates' ) );
+		add_action( 'customize_controls_print_footer_scripts', array( $this, 'print_templates' ), 1 );
 	}
 
 	/**
